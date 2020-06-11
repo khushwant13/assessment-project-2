b07b54a19c2064c63a77d15e900a2ab7cefd91d5
khushwant13 <khushwant.18jics055@jietjodhpur.ac.in>
2020-06-11 18:42:41 +0530

    Health and Care

---
+++ script/facewash.js
@@ -0,0 +1,31 @@
+    var xhr = new XMLHttpRequest();
+    var url = '/json/facewash.json' ;
+    xhr.open('GET', url, true);
+    xhr.onload = function () {
+        if (this.status == 200) {
+            var products = JSON.parse(this.responseText);
+            var output = '';
+            for (var i in products) {
+                output +=
+                '<div class="item">' +
+                '<div class="description">' +
+                '<img src="' + products[i].image + '" width="230" height="300" >' +
+                '<div class="info">' + 
+                '<h2>' + products[i].name + '</h2>' +
+                '<ul>' +
+                '<li>' + products[i].description[0] +  '</li>' +
+                '<li>' + products[i].description[1] +  '</li>' +
+                '<li>' + products[i].description[2] +  '</li>' +
+                '<li>' + products[i].description[3] +  '</li>' +
+                '</ul>' +
+                '</div>' +
+                '</div>' +
+                '<div class="btn">' +
+                '<a href ="' +products[i].url + '">BUY NOW</a>' +
+                '</div>' +
+                '</div>';         
+            }
+            document.getElementById('product').innerHTML = output;
+        }
+    }
+    xhr.send();
