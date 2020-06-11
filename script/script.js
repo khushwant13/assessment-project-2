7a8d041b9e5a8aa656525241c4b7b0bd13d7e189
khushwant13 <65269545+khushwant13@users.noreply.github.com>
2020-06-11 19:03:51 +0530

    Health and Care

--- script/script.js
+++ script/script.js
@@ -6,7 +6,7 @@
     function frame(){
         if(loading == 100){
             clearInterval(id);
-            window.open("/Html/home.html" , "_self")
+            window.open("./Html/home.html" , "_self")
         } else{
             loading = loading + 1;
             if(loading == 90){
